032e6aeaeb7c3b58ad32b8b946786299c7638c2e
William Ledingham <williamledingham1@gmail.com>
2020-01-21 19:57:58 -0700

    Removed TODO in exercise 1. Completed exercise 2 (marathon).

---
+++ Marathon/src/Marathon.java
@@ -0,0 +1,40 @@
+
+public class Marathon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] names = { "Elena", "Thomas", "Hamilton", "Suzie",
+				"Phil", "Matt", "Alex", "Emma", "John", "James", "Jane",
+				"Emily", "Daniel", "Neda", "Aaron", "Kate" 
+				}; 
+		
+		int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243,
+				334, 412, 393, 299, 343, 317, 265
+				}; 
+		
+		Marathon M1 = new Marathon();
+		
+		int fastestIndex = M1.findFastest(names, times);
+		System.out.printf("%s was fastest with a time of %d seconds", names[fastestIndex], times[fastestIndex]);
+
+	}
+	
+	public int findFastest(String[] names, int[] times) 
+	{
+		int index = 0;
+		int fastest = 1000;
+		int fastestIndex = 0;
+		while(index < times.length)
+		{
+			if(times[index] < fastest)
+			{
+				fastest = times[index];
+				fastestIndex = index;
+			}
+			index++;
+		}
+		return fastestIndex;
+	}
+
+}
--- lab1_ex1/src/lab1_ex1/Main.java
+++ lab1_ex1/src/lab1_ex1/Main.java
@@ -3,7 +3,6 @@ package lab1_ex1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		double argsLength = args.length;
 		double sum = 0;
